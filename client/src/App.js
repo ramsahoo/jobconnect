326fa0905b05dce60b9f27187793359a412fdafe
Rameen Sahoo <rameensahoo@gmail.com>
2022-05-28 18:42:19 -0700

    merged changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,44 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 function App() {
-
-  const [backendData, setBackendData] = useState([{}])
+  const [backendData, setBackendData] = useState([{}]);
 
   useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data)
-      }
-    )
-  }, [])
+    fetch("/api")
+      .then((response) => response.json())
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
 
   return (
     <div>
-      
-      {/* {(typeof backendData.users === 'undefined') ? (
+      <div>
+        {/* {(typeof backendData.users === 'undefined') ? (
         <p>Loading...</p>
       ): (
         backendData.users.map((user, i) => (
           <p key={i}>{user}</p>
         ))
       )} */}
-
+      </div>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
